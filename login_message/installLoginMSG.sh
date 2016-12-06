b63152413ded94442102507f8b743d639d98a620
Marcus <mrcsmxms@MacBookPro-Marcus.fritz.box>
2016-12-06 14:00:22 +0100

    follow readme

--- login_message/installLoginMSG.sh
+++ login_message/installLoginMSG.sh
@@ -1,4 +1,4 @@
 cd /home/pi/
-sudo wget https://raw.githubusercontent.com/mrcsmxms/stuff/master/loginMSG.sh
+sudo wget https://raw.githubusercontent.com/mrcsmxms/stuff/master/login_message/loginMSG.sh
 sudo echo "sudo bash /home/pi/loginMSG.sh" >> /etc/profile
 echo "Done!"
